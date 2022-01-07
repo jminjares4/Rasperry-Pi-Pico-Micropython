fdb93592bcca3af61eeae026ed0f630bdb22178a
jminjares4 <jminjares4@miners.utep.edu>
2022-01-07 15:50:35 -0700

    Added PWM

--- Pulse Width Modulation (PWM)/dimming_led.py
+++ Pulse Width Modulation (PWM)/dimming_led.py
@@ -1,9 +1,17 @@
+# Author: Jesus Minjares
+# Master of Science in Computer Engineering
+# Date 01-07-22
+
 import machine
 import utime
 
+# use pin 28 to read ADC
 potentiometer = machine.ADC(28)
+# attach pin 15 to PWM
 led = machine.PWM(machine.Pin(15))
+# set pwm to 1Khz
 led.freq(1000)
 
 while True:
-    led.duty_u16(potentionmeter.read_u16())
+    # update led based on adc reading
+    led.duty_u16(potentiometer.read_u16())
